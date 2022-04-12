9e32cfc46a0eb01c2179d6e3ce9f3fb987e7de42
DongJoo Kwak <40832965+dongzooo@users.noreply.github.com>
2022-04-12 13:02:30 +0900

    큰수의 법칙 풀이 방법 추가

--- 그리디/큰수의 법칙.py
+++ 그리디/큰수의 법칙.py
@@ -1,12 +1,38 @@
-n,m,k = map(int, input().split()) 
-data = list(map(int, input().split()))
+# 단순 for문
+def sol1():
+  n,m,k = map(int, input().split())
 
-data.sort()
+  data = list(map(int, input().split()))
+  result = 0
 
-first = data[n-1] # 가장 큰 수
-second = data[n-2] # 두번쨰로 큰 수
+  data.sort()
+  first = data[-1]
+  second = data[-2]
 
-count = int(m/(k+1)*k + m%(k+1))
+  while True :
+    for i in range(k):
+      if m==0 :
+        break
+      result += first
+      m -= 1
+    if m==0 :
+        break   
+    result += second
+    m -=1
 
-result = first*count + (m-count)*second
-print(result)
+  print(result)
+  
+ # 수열 활용
+def sol2():
+  n,m,k = map(int, input().split()) 
+  data = list(map(int, input().split()))
+
+  data.sort()
+
+  first = data[n-1] # 가장 큰 수
+  second = data[n-2] # 두번쨰로 큰 수
+
+  count = int(m/(k+1)*k + m%(k+1))
+
+  result = first*count + (m-count)*second
+  print(result)
