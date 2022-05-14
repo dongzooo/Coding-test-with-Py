a8c9c6bf27bb11fb601e0be64c55358d0811f7cd
DongJoo Kwak <40832965+dongzooo@users.noreply.github.com>
2022-05-14 22:57:54 +0900

    Create leetcode66.py

---
+++ 구현/leetcode66.py
@@ -0,0 +1,15 @@
+class Solution:
+    def plusOne(self, digits):
+        """
+        Runtime : faster than 61.90% of Python3
+        Memory Usage : less than 5.29% of Python3
+        """
+        digits = list(map(str, digits))
+        nums = int(''.join(digits))
+        nums += 1
+        nums = list(str(nums))
+        return list(map(int, nums))
+ 
+s = Solution()
+print(s.plusOne([1,2,3]))
+print(s.plusOne([4,3,2,1]))
