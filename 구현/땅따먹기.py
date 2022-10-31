9aa4e0a43e432317f9e27bdaff7323687c056228
DongJoo Kwak <40832965+dongzooo@users.noreply.github.com>
2022-11-01 01:01:37 +0900

    Create 땅따먹기.py

---
+++ 구현/땅따먹기.py
@@ -0,0 +1,11 @@
+def solution(land):
+    answer = 0
+    for i in range(1,len(land)):
+        print('ex',land)
+        land[i][0] += max(land[i-1][1], land[i-1][2], land[i-1][3])
+        land[i][1] += max(land[i-1][0], land[i-1][2], land[i-1][3])
+        land[i][2] += max(land[i-1][0], land[i-1][1], land[i-1][3])
+        land[i][3] += max(land[i-1][0], land[i-1][1], land[i-1][2])
+        print('af',land)
+    print(land[-1])
+    return max(land[-1])
