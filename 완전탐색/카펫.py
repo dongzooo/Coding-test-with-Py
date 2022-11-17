f2adcd8e1f049b6edf6b987e6635bbbee43cfb25
DongJoo Kwak <40832965+dongzooo@users.noreply.github.com>
2022-11-17 15:47:49 +0900

    Update 카펫.py

--- 완전탐색/카펫.py
+++ 완전탐색/카펫.py
@@ -1,17 +1,14 @@
-#둘레는 brown + 4이다, 가로 >= 세로
-# w>=h , 2w+2h = brown +4, brown + yellow = 넓이
-# 넓이/가로 = 세로
 def solution(brown, yellow):
     answer = []
-    for h in range(brown//2):
-        for w in range(h,brown//2+1):
-            if w >= h :
-                if w*h == (brown + yellow)and 2*w+2*h == brown +4 :
-                    answer = [w,h]
-                    return answer
-                    
-    return answer   
+    #넓이 = brown + yellow = w*h
+    #       w = (brown + yellow)/h
+    #둘레 = brown+4 = 2w+2h
+    #w >= h
+    for h in range(1,brown//2):
+        w = (brown + yellow)/h
+        if w >= h :
+            if brown+4 == 2*(w+h) :
+                answer = [w,h]
+                break
     
-    
-        
-    
+    return answer
